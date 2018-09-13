8bcfb57d275568046c83c52c83740d5af798751f
merisargsyan2 <mary.mary.sargsyann@gmail.com>
2018-09-13 18:38:23 +0400

    first commit

---
+++ homework2.js
@@ -0,0 +1,37 @@
+console.log("hiiiii");
+alert("Welcome") 
+
+const firstName = 'Meri';
+const lastName = 'Sargsyan';
+
+const a = 45 - 35;
+const b = (32 + 3)* 2;
+
+const num = 765;
+const str = 'CS110 is very interesting';
+
+	console.log(firstName +' '+ lastName);
+	
+if(a !== b) {
+ 	console.log(str);
+ }
+
+ console.log((1900 + 118) / 1);
+
+if(a > b) {
+	console.log('Nope');
+ }  else {
+ 	console.log("IESM");
+ 	}
+
+ if(num === 765 - 3) {
+ 	console.log('Yaa'); 
+ } else {
+ 		console.log('Yeeey');
+}
+
+    
+
+
+
+
